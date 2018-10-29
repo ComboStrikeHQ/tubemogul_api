156c9a7d974f313337cd1d0f6ba32890c3712317
Guilherme Goettems Schneider <guigs81@gmail.com>
2018-10-29 15:10:06 -0300

    Refactor Anonymize module

--- spec/support/anonymize.rb
+++ spec/support/anonymize.rb
@@ -2,36 +2,25 @@
 
 module Anonymize
   def advertiser(record, id)
-    record.each_key do |key|
-      case key
-      when 'advertiser_id'
-        record[key] = id
-      when '@uri'
-        record[key] = record[key].sub(%r{(trafficking\/advertisers\/)([^\?]+)}, "\\1#{id}")
-      when 'advertiser_name'
-        record[key] = "Advertiser #{id}"
-      when 'advertiser_domain'
-        record[key] = "https://#{id}.example.com"
-      end
-    end
+    replace(
+      record,
+      'advertiser_id' => id,
+      '@uri' => record['@uri'].sub(%r{(trafficking\/advertisers\/)([^\?]+)}, "\\1#{id}"),
+      'advertiser_name' => "Advertiser #{id}",
+      'advertiser_domain' => "https://#{id}.example.com"
+    )
   end
   module_function :advertiser
 
   def campaign(record, id)
-    record.each_key do |key|
-      case key
-      when '@uri'
-        record[key] = record[key].sub(%r{(trafficking\/campaigns\/)([^\?]+)}, "\\1#{id}")
-      when 'campaign_id'
-        record[key] = id
-      when 'campaign_key'
-        record[key] = "key#{id}"
-      when 'advertiser_id'
-        record[key] = 1
-      when 'campaign_name'
-        record[key] = "Campaign #{id}"
-      end
-    end
+    replace(
+      record,
+      '@uri' => record['@uri'].sub(%r{(trafficking\/campaigns\/)([^\?]+)}, "\\1#{id}"),
+      'campaign_id' => id,
+      'campaign_key' => 'key1',
+      'advertiser_id' => 1,
+      'campaign_name' => "Campaign #{id}"
+    )
   end
   module_function :campaign
 
@@ -40,28 +29,21 @@ module Anonymize
       puts 'Add to your .env.test file:'
       puts "SAMPLE_PLACEMENT_ID: '#{item['placement_id']}'"
     end
-    record.each_key do |key|
-      case key
-      when 'placement_id'
-        record[key] = id
-      when 'placement_key'
-        record[key] = "placementkey#{id}"
-      when '@uri'
-        if record['@type'] == 'placement'
-          record[key] = record[key].sub(%r{(trafficking\/placements\/)([^\?]+)}, "\\1#{id}")
-        elsif record['@type'] == 'placement_report'
-          record[key] = record[key].sub(%r{(reporting\/placements\/)\d+(\??.*)}, "\\1#{id}\\2")
-        end
-      when 'placement_name'
-        record[key] = "Placement #{id}"
-      when 'campaign_id'
-        record[key] = '1'
-      when 'campaign_key'
-        record[key] = 'key1'
-      when 'campaign_name'
-        record[key] = 'Campaign 1'
-      end
+    uri = if record['@type'] == 'placement'
+            record['@uri'].sub(%r{(trafficking\/placements\/)([^\?]+)}, "\\1#{id}")
+          elsif record['@type'] == 'placement_report'
+            record['@uri'].sub(%r{(reporting\/placements\/)\d+(\??.*)}, "\\1#{id}\\2")
     end
+    replace(
+      record,
+      'placement_id' => id,
+      'placement_key' => "placementkey#{id}",
+      '@uri' => uri,
+      'placement_name' => "Placement #{id}",
+      'campaign_id' => 1,
+      'campaign_key' => 'key1',
+      'campaign_name' => 'Campaign 1'
+    )
   end
   module_function :placement
 
@@ -70,20 +52,21 @@ module Anonymize
       puts 'Add to your .env.test file:'
       puts "SAMPLE_MEDIA_PLACEMENT_ID: '#{record['media_placement_id']}'"
     end
-    record.each_key do |key|
-      case key
-      when 'media_placement_id'
-        record[key] = id
-      when '@uri'
-        record[key] = record[key].sub(%r{(reporting\/media_placements\/)\d+(\??.*)}, "\\1#{id}\\2")
-      when 'media_placement_name'
-        record[key] = "Media Placement #{id}"
-      when 'media_property_id'
-        record[key] = 'media-property-id'
-      when 'media_property_name'
-        record[key] = 'Media Property Name'
-      end
-    end
+    replace(
+      record,
+      'media_placement_id' => id,
+      '@uri' => record['@uri'].sub(%r{(reporting\/media_placements\/)\d+(\??.*)}, "\\1#{id}\\2"),
+      'media_placement_name' => "Media Placement #{id}",
+      'media_property_id' => 'media-property-id',
+      'media_property_name' => 'Media Property Name'
+    )
   end
   module_function :media_placement
+
+  def replace(original, replacement)
+    replacement.each do |key, value|
+      original[key] = value if original.key?(key)
+    end
+  end
+  module_function :replace
 end
